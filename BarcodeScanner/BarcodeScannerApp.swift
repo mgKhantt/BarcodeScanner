126c3c0e4ee34aed43a9716a3eef5d51913fde28
mgKhantt <khantphonenaing156@gmail.com>
2024-11-30 14:32:42 +0630

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Khant Phone Naing  on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScannerTests/BarcodeScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerTests.swift
+//  BarcodeScannerTests
+//
+//  Created by Khant Phone Naing  on 30/11/2024.
+//
+
+import Testing
+@testable import BarcodeScanner
+
+struct BarcodeScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
